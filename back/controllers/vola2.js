cc03affb0f3852e19f3b075a332e997b2e9e3a74
OscarD7 <oscar.castillo@epn.edu.ec>
2023-08-09 10:02:19 -0500

    feat vuelo buscar feature

--- back/controllers/vola2.js
+++ back/controllers/vola2.js
@@ -22,6 +22,16 @@ getVuelos: async function(req, res){
     }
 },
 
+getVuelosBuscar: async function(req, res){
+    try{
+        const vuelos = await Vuelo.find({origen: req.params.origen, destino: req.params.destino, fecha_salida: req.fecha_salida, asientos_disponibles: req.asientos_disponibles }).sort();
+        if (!vuelos) return res.status(404).send({message: 'No hay vuelos'});
+        return res.status(200).send({vuelos});
+    }catch(err){
+        return res.status(500).send({message: "Error al devolver los datos"});
+    }
+},
+
 saveVuelo: async function(req, res){
     try{
         var vuelo = new Vuelo();
