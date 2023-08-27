349a08b48af985c737bec72e868782877ae64f3d
Adhisson Cedeño <adhisson89@hotmail.com>
2023-08-26 20:35:29 -0500

    fix: routing

--- front/app-web-vola2/src/app/app-routing.module.ts
+++ front/app-web-vola2/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/user/home/home.component';
 import { VuelosComponent } from './modules/user/vuelos/vuelos.component';
+import {PersonalizacionComponent} from "./modules/user/personalizacion/personalizacion.component";
 
 
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path:'vuelos', component:VuelosComponent},
+  {path:'personalizacion', component:PersonalizacionComponent},
   {path:'**', component:HomeComponent}//ruta 404 no se carga el componente
 ];
 
